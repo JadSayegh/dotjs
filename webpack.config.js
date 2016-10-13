de08f6b5752ccc7d565b483991e03e6c864221a8
Jad Sayegh <jadsayegh@Jads-MacBook-Pro.local>
2016-10-12 20:31:07 -0400

    dev environment setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/static/public');
+var APP_DIR = path.resolve(__dirname, 'src/static');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }  
+    ]
+  }
+};
+
+module.exports = config;
